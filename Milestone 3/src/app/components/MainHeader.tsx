c849daf90afe627357e1a5b2d2c7d4457bd090c6
Hurmat Ayub <156796678+hurmatayub@users.noreply.github.com>
2025-01-06 01:47:41 +0500

    Update MainHeader.tsx

--- Milestone 3/src/app/components/MainHeader.tsx
+++ Milestone 3/src/app/components/MainHeader.tsx
@@ -1,54 +1,63 @@
-import React from 'react'
-import { BiUser } from 'react-icons/bi'
-import { BsSearch } from 'react-icons/bs'
-import { FiHeart } from 'react-icons/fi'
-import { HiOutlineShoppingBag } from 'react-icons/hi'
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
 
-const MainHeader = () => {
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className='border-b border-gray-200 py-6'>
-      <div className='container sm:flex justify-between items-center'>
-        <div className='font-bold text-4xl text-center pb-4 sm:pb-0 text-blackish font-mono'>
-          Adornia
+    <nav className="bg-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="md:hidden flex items-center" onClick={toggleMenu}>
+          <Image src="/Menu.png" alt="" width={24} height={24} />
+        </div>
+        <div className="text-black font-bold text-xl p-2 ml-11">CeramiLine</div>
+
+        <div className="hidden lg:flex  gap-20 justify-center flex-1 text-[#3A3845]">
+          <a href="#">Home</a>
+          <a href="#">Shop</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
         </div>
 
-        <div className='w-full sm:w-[300px] md:w-[70%] relative'>
-          <input
-            className='border-gray-200 border p-2 px-4 rounded-lg w-full'
-            type='text' placeholder='Enter Any Product name...' />
-
-          <BsSearch 
-          className='absolute right-0 top-0 mr-3 mt-3 text-gray-400 size{20}'/>
-        </div>
-
-        <div className='hidden lg:flex gap-4 text-gray-500 text-[30px]'>
-          <BiUser />
-
-         <div className='relative'>
-
-          <FiHeart />
-          <div className='bg-red-600 rounded-full absolute top-0 right-0 w-[18px] h-[18px] text-[12px] text-white grid place-items-center translate-x-1 translate-y-1'>
-            0
+        <div className="flex gap-1 ml-4">
+          <div className="lg:flex ml-4 hidden sm:block">
+            <Image src="/Search.png" alt="Search" width={24} height={24} />
           </div>
-         </div>
-
-          <div className='relative'>
-          <HiOutlineShoppingBag />
-          <div className='bg-red-600 rounded-full absolute top-0 right-0 w-[18px] h-[18px] text-[12px] text-white grid place-items-center translate-x-1 translate-y-1'>
-            0
+          <div className="lg:flex ml-4 hidden sm:block">
+            <Image src="/User.png" alt="User" width={24} height={24} />
           </div>
-         </div>
+          <div className="lg:flex ml-4 hidden sm:block">
+            <Image src="/Heart.png" alt="Heart" width={24} height={24} />
+          </div>
+
+          <div className="flex space-x-4 ml-4 md:flex">
+            <Image src="/Shopping cart.png" alt="" width={24} height={24} />
+          </div>
         </div>
-        
-
-         
-          
-        
-
       </div>
-   </div>
-  )
-}
+      {isMenuOpen && (
+        <div className="flex flex-col items-center space-y-4 mt-4">
+          <a href="#" className="text-black">
+            Home
+          </a>
+          <a href="#" className="text-black">
+            Shop
+          </a>
+          <a href="#" className="text-black">
+            About
+          </a>
+          <a href="#" className="text-black">
+            Contact
+          </a>
+        </div>
+      )}
+    </nav>
+  );
+};
 
-export default MainHeader
-
+export default Navbar;
